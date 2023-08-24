d98a6333dbdb51086e9b2140ad7a57b16cf17265
SuchitraDeviR <142719000+SuchitraDeviR@users.noreply.github.com>
2023-08-24 20:36:42 +0530

    Add files via upload

---
+++ homeassignment/week1/PrintPositiveNegativeNumbers.java
@@ -0,0 +1,25 @@
+package homeassignment.week1;
+
+public class PrintPositiveNegativeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number=-10;
+		
+		if(number<0)
+		{
+			System.out.println("The Negative number is "+ number);
+		}
+		else if(number>0)
+		{
+			System.out.println("The positive number is " + number);
+		}
+		else
+		{
+			System.out.println("The number is neither positive nor negative");
+		}
+			
+	}
+
+}
